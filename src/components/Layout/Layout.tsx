7bc8a1990510fffe17cf8171019c92a617f0f387
Janne <jabbekeipert@gmail.com>
2025-10-23 20:27:09 +0200

    Enhance Layout component with GSAP animations for mobile-not-supported section; refactor structure for improved readability

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,44 +1,63 @@
 import type { ReactNode } from "react";
 import { Header } from "@/components/Header/Header.tsx";
 import { HeroSection } from "@/components/HeroSection/HeroSection.tsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 interface LayoutProps {
-	children: ReactNode;
-	mode?: "small" | "default";
+  children: ReactNode;
+  mode?: "small" | "default";
 }
 
 export const Layout = ({ children, mode }: LayoutProps) => {
-	return (
-		<div
-			className={
-				"w-full h-full min-h-screen flex items-center justify-center text-white dark overflow-x-hidden " +
-				(mode == "small" ? "pb-[200px]" : "")
-			}
-			style={{
-				background:
-					"radial-gradient(128.47% 70.71% at 50% 50%, var(--background-from) 27.4%, var(--background-to) 100%)",
-			}}
-		>
-			<div className="flex flex-col items-center justify-center h-fit px-4 max-w-full">
-				<div className="flex flex-col md:flex-row gap-y-5 md:gap-y-0 items-center justify-center text-center space-x-5 max-w-full py-[30px] lg:pt-[100px]">
-					<Header small={mode == "small"} />
+  useGSAP(() => {
+    gsap.fromTo(".mobile-not-supported-section",
+      {
+        opacity: 0,
+        top: "10px"
+      },
+      {
+        opacity: 1,
+        delay: 1.3,
+        top: "0px",
+        ease: "power2.out",
+        duration: 0.3
+      });
+    console.log("gsap");
+  });
 
-					<HeroSection
-						subtitle="Get Yourself"
-						title="Covered"
-						tagline="Customize your Steam Library"
-					/>
-				</div>
+  return (
+    <div
+      className={
+        "w-full h-full min-h-screen flex items-center justify-center text-white dark overflow-x-hidden " +
+        (mode === "small" ? "pb-[200px]" : "")
+      }
+      style={{
+        background:
+          "radial-gradient(128.47% 70.71% at 50% 50%, var(--background-from) 27.4%, var(--background-to) 100%)"
+      }}
+    >
+      <div className="flex flex-col items-center justify-center h-fit px-4 max-w-full">
+        <div
+          className="flex flex-col md:flex-row gap-y-5 md:gap-y-0 items-center justify-center text-center space-x-5 max-w-full py-[30px] lg:pt-[100px]">
+          <Header small={mode === "small"} />
 
-				<div className={"relative hidden lg:block"}>{children}</div>
-				<div
-					className={
-						"relative lg:hidden text-gray-500 text-xl py-[50px] pb-[100px] text-center w-[70%]"
-					}
-				>
-					This app is not available on mobile devices.
-				</div>
-			</div>
-		</div>
-	);
+          <HeroSection
+            subtitle="Get Yourself"
+            title="Covered"
+            tagline="Customize your Steam Library"
+          />
+        </div>
+
+        <div className={"relative hidden lg:block"}>{children}</div>
+        <div
+          className={
+            "relative lg:hidden text-gray-500 text-xl py-[50px] pb-[100px] text-center w-[70%] mobile-not-supported-section opacity-0"
+          }
+        >
+          This app is not available on mobile devices.
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/ProgressAnimation.tsx
+++ src/components/ProgressAnimation.tsx
@@ -1,10 +1,10 @@
 import { Progress as ProgressPrimitive } from "radix-ui";
 
 const ProgressAnimation = (props: { progress: number }) => {
-	return (
-		<div className="w-[60%]">
-			<style>
-				{`@keyframes progress {
+  return (
+    <div className="w-[60%]">
+      <style>
+        {`@keyframes progress {
             to {
               left: calc(100% - 2rem);
             }
@@ -14,17 +14,17 @@ const ProgressAnimation = (props: { progress: number }) => {
             animation: progress 1.25s ease-in-out infinite;
           }
           `}
-			</style>
-			<ProgressPrimitive.Root className="relative h-2 w-full overflow-hidden rounded-full bg-primary/20">
-				<ProgressPrimitive.Indicator
-					className="relative h-full w-full flex-1 bg-primary transition-all"
-					style={{ transform: `translateX(-${100 - (props.progress || 0)}%)` }}
-				>
-					<div className="absolute left-0 w-6 h-full bg-primary-foreground blur-[10px] inset-y-0 progress" />
-				</ProgressPrimitive.Indicator>
-			</ProgressPrimitive.Root>
-		</div>
-	);
+      </style>
+      <ProgressPrimitive.Root className="relative h-2 w-full overflow-hidden rounded-full bg-primary/20">
+        <ProgressPrimitive.Indicator
+          className="relative h-full w-full flex-1 bg-primary transition-all"
+          style={{ transform: `translateX(-${100 - (props.progress || 0)}%)` }}
+        >
+          <div className="absolute left-0 w-6 h-full bg-primary-foreground blur-[10px] inset-y-0 progress" />
+        </ProgressPrimitive.Indicator>
+      </ProgressPrimitive.Root>
+    </div>
+  );
 };
 
 export default ProgressAnimation;
